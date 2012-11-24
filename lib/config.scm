e3e1e027e09c53f827f84a0c2a1dad0439e19529
mrk25 <mrk25@keyframe.local>
2012-11-24 17:34:17 +0100

    OSX audio config added

--- lib/config.scm
+++ lib/config.scm
@@ -1,6 +1,10 @@
 (clear)
 (reset-camera)
+; Debian GNU/Linux
 (start-audio "alsa_pcm:capture_1" 1024 44100)
+
+; OSX
+;(start-audio "system:capture_1" 1024 44100)
 (gain 0.1)
 
 (load (string-append base_dir "lib/objects.scm"))
